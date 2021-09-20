5d84e940bcc33597d119c33c419716393967ee3d
0b1kn00b <polysemantic@gmail.com>
2021-09-20 13:46:09 +0200

    working on cleaner traces

--- src/main/haxe/stx/assert/comparable/term/Base.hx
+++ src/main/haxe/stx/assert/comparable/term/Base.hx
@@ -1,10 +1,11 @@
 package stx.assert.comparable.term;
 
-class Base<T> implements ComparableApi<T>{
+class Base<T> implements ComparableApi<T> extends Clazz{
   var _eq : Eq<T>;
   var _lt : Ord<T>;
 
   public function new(_eq,_ord){
+    super();
     this._eq    = _eq;
     this._lt    = _ord;
   }
@@ -13,5 +14,8 @@ class Base<T> implements ComparableApi<T>{
   }
   public function lt() : Ord<T>{
     return _lt;
+  }
+  public function toString(){
+    return identifier(); 
   }
 }
