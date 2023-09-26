097ce06a13deca3e2e3f0cc5d8ed7422cdb4a3f4
0b1kn00b <polysemantic@gmail.com>
2023-09-26 09:29:11 +0200

    hx -> hb

--- src/main/haxe/stx/assert/eq/term/String.hx
+++ src/main/haxe/stx/assert/eq/term/String.hx
@@ -1,6 +1,9 @@
 package stx.assert.eq.term;
 
 class String extends EqCls<StdString>{
+  static public function Unit(){
+    return new String();
+  }
   public function new(){}
   public function comply(a:StdString,b:StdString):Equaled{
     return a == b ? AreEqual : NotEqual;
